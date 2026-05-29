3a9b756e439a508101d1f921a7817204425413fa
Pinocchio <taha.mankir123@gmail.com>
2026-05-29 03:17:45 +0300

    Sync

--- lua/config/sets.lua
+++ lua/config/sets.lua
@@ -1,4 +1,4 @@
-vim.cmd("colorscheme onedark")
+vim.cmd("colorscheme carbonfox")
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
@@ -26,3 +26,18 @@ vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
+
+-- Ana arka plan
+vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" })
+
+-- Odakta olmayan pencereler (opsiyonel ama tutarlılık için gerekli)
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "#000000" })
+
+-- Sol taraftaki işaret sütunu (satır numaralarının yanı)
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "#000000" })
+
+-- Kayan pencereler (Telescope, lsp hover vb.)
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#000000" })
+
+-- Dosya sonundaki tilde (~) işaretlerinin arka planı
+vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "#000000" })
